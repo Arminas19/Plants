b1656c9bdc5341b5618ffc3fbc11f6d4b283d7e0
Arminas19 <armeska14@gmail.com>
2022-10-12 16:01:23 +0000

    added plants layout to javascript.

---
+++ JS/javaScript.js
@@ -0,0 +1,36 @@
+// function change() {
+// let fruit = ['apple', 'bananna', 'orange', 'graps'];
+// fleng = fruit.length
+    
+// fruit[0] = 'lemon'
+// let text = '<ul>';
+// for(i = 0; i < fleng; i++) {
+// text+= '<li>' + fruit[i] + '</li>';
+// }
+// text += '</ul>';
+// document.getElementById('demo1').innerHTML = "Changed JavaScript.";
+// document.getElementById('demo1').style.color = "red";
+// document.getElementById('demo2').innerHTML = "Changed Text.";
+// document.getElementById('demo3').innerHTML = text;
+// console.log(text);
+
+// };
+    
+
+// let fruit = ['apple', 'bananna', 'orange', 'graps'];
+// fleng = fruit.length
+// let text = '<ul>';
+// for(i = 0; i < fleng; i++) {
+// text+= '<li>' + fruit[i] + '</li>';
+// }
+// text += '</ul>';
+// function NewChange() {
+// document.getElementById('demo1').style.color = "cyan";
+// document.getElementById('demo1').innerHTML = "Timmy Gay";
+// document.getElementById('demo2').innerHTML = "Struna Fat Plum";
+// document.getElementById('demo2').style.color = "yellow";
+// document.getElementById('demo3').innerHTML = 'You stick theys in your bum (struna & Timmy)' + text;
+// document.getElementById('demo3').style.fontSize = '40px';
+// document.getElementById('demo3').style.color = 'limegreen';
+
+// };
