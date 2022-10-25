d4c1780e38b00001c9e93874731bf7b00018fd2a
Arminas19 <armeska14@gmail.com>
2022-10-25 12:05:55 +0000

    testing some jQuery code out.

--- assets/JS/test.js
+++ assets/JS/test.js
@@ -4,4 +4,10 @@ $(document).ready(function() {
     $("p.custom-class").text("Changed Again");
     $("#paraId").text("LoL sO Funny");
     $("p span strong ").text("Stronk");
+
+    $('#customId').html('<h2 class="color-red"> Red </h2>');
+    let blue = $('.color-red').html('<h2 class="color-blue"> Blue </h2>');
+
+    $('#customId').text('<p> </p> VS  <span>' + blue);
+
 });
